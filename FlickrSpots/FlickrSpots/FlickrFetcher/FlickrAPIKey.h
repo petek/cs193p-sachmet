818fb4cc0ca0fba8f489dceef40fa0dddb5da8c2
Pete Krawczyk <github@bsod.net>
2012-10-31 11:39:48 -0500

    Add FlickrFetcher API

---
+++ FlickrSpots/FlickrSpots/FlickrFetcher/FlickrAPIKey.h
@@ -0,0 +1,13 @@
+//
+//  FlickrAPIKey.h
+//
+//  Created for Stanford CS193p Fall 2011.
+//  Copyright 2011 Stanford University. All rights reserved.
+//
+//  Get your own key!
+//  No Flickr fetches will work without the API Key!
+//
+//  When you update the key, it would probably be best to do this:
+//  git update-index --assume-unchanged FlickrSpots/FlickrSpots/FlickrFetcher/FlickrAPIKey.h
+
+#define FlickrAPIKey @""
