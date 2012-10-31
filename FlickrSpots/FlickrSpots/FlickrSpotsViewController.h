81347a77b3b5ee9a74fa8f47faeb44ef0634d4e2
Krawczyk <pkrawczyk@mhflflrw16xrze4.na.follett.lan>
2012-10-31 11:15:56 -0500

    Initial code

---
+++ FlickrSpots/FlickrSpots/FlickrSpotsViewController.h
@@ -0,0 +1,13 @@
+//
+//  FlickrSpotsViewController.h
+//  FlickrSpots
+//
+//  Created by Krawczyk, Pete on 10/31/12.
+//  Copyright (c) 2012 Krawczyk, Pete. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlickrSpotsViewController : UIViewController
+
+@end
