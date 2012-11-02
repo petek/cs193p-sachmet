7db4e46cc6ac2123ed3f3068ee7a230e8d3863b6
Pete Krawczyk <github@bsod.net>
2012-11-02 09:30:33 -0500

    Clean up garbage controllers, move files back into the right place

--- FlickrSpots/FlickrSpots/FlickrSpotsRecentPhotosViewController.h
+++ FlickrSpots/FlickrSpots/FlickrSpotsRecentPhotosViewController.h
@@ -1,13 +1,21 @@
 //
-//  FlickrSpotsSecondViewController.h
+//  FlickrSpotsRecentPhotosViewController.h
 //  FlickrSpots
 //
-//  Created by Krawczyk, Pete on 10/31/12.
+//  Created by Krawczyk, Pete on 11/1/12.
 //  Copyright (c) 2012 Krawczyk, Pete. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface FlickrSpotsRecentPhotosViewController : UIViewController
+@class FlickrSpotsRecentPhotosViewController;
+
+@protocol FlickrSpotsRecentPhotosViewControllerDelegate <NSObject>;
+@optional
+@end
+
+@interface FlickrSpotsRecentPhotosViewController : UITableViewController
+
+@property (nonatomic, weak) id delegate;
 
 @end
